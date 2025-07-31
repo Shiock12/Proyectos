f0c95106de23d6276c4465fac56ac51becc166a6
Shiock12 <84999409+Shiock12@users.noreply.github.com>
2025-07-31 16:15:51 -0300

    Update js
    Borre los comentarios nomas

--- Proyecto Lista/index.js
+++ Proyecto Lista/index.js
@@ -14,27 +14,6 @@ items.forEach(item => {
     listaUL.appendChild(li);
 });
 
-/*
-button.addEventListener("click", () =>{
-    const input = document.getElementById("items").value; // Capturo el valor
-    const listaUL = document.getElementById("lista");
-
-    if(input === ""){
-        alert("No ingresaste ningun elemento");
-    }else{
-        let nuevoitem = document.createElement("li");
-        nuevoitem.textContent = input;
-        items.push(input);
-        localStorage.setItem("items",JSON.stringify(items));
-        
-        nuevoitem.addEventListener("click", function() {
-            this.remove();
-        });
-        listaUL.appendChild(nuevoitem);
-        inputfield.value = "";
-    }
-});
-*/
 inputfield.addEventListener("keydown",(event) =>{
     if(event.key === "Enter"){
         const input = document.getElementById("items").value; // Capturo el valor
